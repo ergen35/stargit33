9adabcc27c4e8fae5ed31f7d7e47446f4035e392
Karim5656 <76402394+Karim5656@users.noreply.github.com>
2023-03-01 12:12:09 +0100

    Added PersonsController (#5)
    * Added PersonsController
    
    * Various updates

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MinimalAPIS.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<PersonContext>(opt =>
+    opt.UseInMemoryDatabase("Minimals"));
 
 var app = builder.Build();
 
@@ -14,6 +19,9 @@ app.MapGet("/get_json", (req) =>
     return req.Response.WriteAsJsonAsync(new { hello = "barnabew" });
 });
 
+
+
 app.MapControllers();
+
 
 app.Run();
